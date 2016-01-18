11d8217c84e780a82f4d6375c319558a1e688a67
florije1988 <florije1988@gmail.com>
2016-01-18 17:41:04 +0800

    1_2

--- 1_2.py
+++ 1_2.py
@@ -1,2 +1,15 @@
 # -*- coding: utf-8 -*-
 __author__ = 'florije'
+
+
+class C:
+    def __init__(self, arg1, arg2):
+        self.str = arg1
+        self.lst = arg2
+
+
+iC = C("arun", [1, 2])
+print(iC.str)
+print(iC.lst)
+iC.lst.append(4)
+print(iC.lst)
