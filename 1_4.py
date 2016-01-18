1e324474cbbbcb6fbc006fbef7beb4679ff18921
florije1988 <florije1988@gmail.com>
2016-01-18 18:12:52 +0800

    1_4,看不懂c()调用是什么意思。

--- 1_4.py
+++ 1_4.py
@@ -27,3 +27,33 @@ c.__call__()
 c.__class__.__call__(c)
 print(callable(lambda x: x + 1))
 print(isinstance(lambda x: x + 1, collections.Callable))
+
+
+class D:
+    pass
+
+
+class D1:
+    def do(self, ):
+        print("do run", self)
+
+
+def doo(obj):
+    print("doo run", obj)
+
+
+d1 = D1()
+d = D()
+
+doo(d1)
+doo(d)
+
+d1.do()
+
+D1.doo = doo
+d1.doo()
+D1.doo()
+D1.do()
+D1.do(d1)
+D1.do(d)
+d1.do.__func__(d)
