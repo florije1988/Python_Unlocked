699c0734eb75a04519300a305d0f5d04cc656d8c
florije1988 <florije1988@gmail.com>
2016-02-15 17:24:50 +0800

    1_4,看不懂c()调用是什么意思。

---
+++ coroutines/test_gen.py
@@ -0,0 +1,18 @@
+# -*- coding: utf-8 -*-
+__author__ = 'florije'
+
+
+# def countdown(n):
+#     while n > 0:
+#         yield n
+#         # n -= 1
+#
+#
+# for i in countdown(10):
+#     print(i)
+
+
+count = 1
+while count > 0:
+    print(count)
+    count -= 1
---
+++ fib.py
@@ -0,0 +1,11 @@
+# -*- coding: utf-8 -*-
+__author__ = 'florije'
+
+from log_execution_time import log_execution_time
+
+
+def fib(n):
+    return fib(n - 1) + fib(n - 2) if n > 1 else n
+
+
+timed_fib = log_execution_time(fib)
